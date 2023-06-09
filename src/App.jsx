03132cff4e7b95fdbc39438d97f1581242f797af
YagoSilva <yagohenriquest@gmail.com>
2023-06-09 10:39:36 -0300

    started project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import {Post} from './Post';
+
+export function App() {
+  
+  return (
+    <div>
+      <Post author="Yago Henrique" content="lorem ipsum avit amer sidum oberas sidok"/>
+    </div>
+  )
+}
+
